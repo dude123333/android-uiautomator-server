2f3580daa2925459ba274ca1020c759607430c4c
dude123333 <145718331+dude123333@users.noreply.github.com>
2024-07-15 03:29:17 +0300

    Update ConfigReader.java

--- app/src/androidTest/java/com/github/uiautomator/stub/ConfigReader.java
+++ app/src/androidTest/java/com/github/uiautomator/stub/ConfigReader.java
@@ -5,13 +5,17 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-
-    public static int getPortFromConfig() throws IOException {
+    public static int getPortFromConfig() {
         Properties properties = new Properties();
-        FileInputStream inputStream = new FileInputStream("/sdcard/Android/data/com.github.uiautomator/files/config.properties");
-        properties.load(inputStream);
-        int port = Integer.parseInt(properties.getProperty("port"));
-        inputStream.close();
+        int port = 9008;
+        try {
+            FileInputStream inputStream = new FileInputStream("/sdcard/config.properties");
+            properties.load(inputStream);
+            port = Integer.parseInt(properties.getProperty("port", "9008"));
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return port;
     }
 }
